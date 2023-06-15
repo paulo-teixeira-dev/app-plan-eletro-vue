d740918a179eebdba36d2084ea237bad4d04033f
Código Storm <codigostorm.adm@gmail.com>
2023-06-15 13:11:32 -0300

    redirect index

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -3,6 +3,10 @@ import {createRouter, createWebHistory} from 'vue-router'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: '/',
+            redirect: '/eletro'
+        },
         /* -- ELETRO -- */
         {
             path: '/eletro',
